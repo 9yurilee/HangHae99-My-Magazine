8a448478249546c637922bc7558f1cba5c67da08
degurrrrrr <degurrrrrr@gmail.com>
2022-02-08 15:09:02 +0900

    2/8 게시글 목록 작성일 기준으로 가져오기
    post.js > getPostFB

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -147,26 +147,25 @@ const getPostFB = () => {
   return function (dispatch, getState, { history }) {
     const postDB = firestore.collection('post');
 
-    postDB.get().then((docs) => {
-      let post_list = [];
+    let query = postDB.orderBy("insert_dt", "desc").limit(2);
 
+  query.get().then(docs => {
+      let post_list = [];
       docs.forEach((doc) => {
-        let _post = {
-          id: doc.id,
-          ...doc.data(),
-        };
-        let post = {
-          id: doc.id,
-          user_info: {
-            user_name: _post.user_name,
-            user_profile: _post.user_profile,
-            user_id: _post.user_id,
+        let _post = doc.data();
+        
+        let post = Object.keys(_post).reduce(
+          (acc, cur) => {
+            if (cur.indexOf("user_") !== -1) {
+              return {
+                ...acc,
+                user_info: { ...acc.user_info, [cur]: _post[cur] },
+              };
+            }
+            return { ...acc, [cur]: _post[cur] };
           },
-          contents: _post.contents,
-          image_url: _post.image_url,
-          comment_cnt: _post.comment_cnt,
-          imsert_dt: _post.insert_dt,
-        };
+          { id: doc.id, user_info: {} }
+        );
 
         post_list.push(post);
       });
